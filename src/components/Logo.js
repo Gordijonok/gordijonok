0f7d411e9ea6e03d1d65cf29ca87932e175dc50b
Gordijonok <dkflbvbhujhlb@mail.ru>
2023-09-14 20:17:36 +0300

    убрал дублирование

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,41 +3,22 @@ import sun from "../img/sun.png";
 import moon from "../img/moon.png";
 
 function Logo({ black, setBlack }) {
-  if (black) {
-    return (
-      <div class="logo">
-        <a href="/">
-          <img className="logo__home" src={home} alt="home" />
-        </a>
-        <a href="#">
-          {" "}
-          <img
-            className="logo__sun"
-            onClick={() => setBlack((black) => !black)}
-            src={sun}
-            alt="sun"
-          />
-        </a>
-      </div>
-    );
-  } else {
-    return (
-      <div class="logo">
-        <a href="/">
-          <img className="logo__home" src={home} alt="home" />
-        </a>
-        <a href="#">
-          {" "}
-          <img
-            className="logo__sun"
-            onClick={() => setBlack((black) => !black)}
-            src={moon}
-            alt="sun"
-          />
-        </a>
-      </div>
-    );
-  }
+  return (
+    <div class="logo">
+      <a href="/">
+        <img className="logo__home" src={home} alt="home" />
+      </a>
+      <a href="#">
+        {" "}
+        <img
+          className="logo__sun"
+          onClick={() => setBlack((black) => !black)}
+          src={black ? sun : moon}
+          alt="sun"
+        />
+      </a>
+    </div>
+  );
 }
 
 export { Logo };
