3cc85c8c5b571a6b4f9fbde9f2127a4fa25165e4
Gordijonok <dkflbvbhujhlb@mail.ru>
2023-09-17 17:44:25 +0300

    фон + ошибка

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,11 @@
+import { useContext } from "react";
+
+import { ThemeContext } from "./ThemeProvider";
+
 function Footer() {
+  const { isDark } = useContext(ThemeContext);
   return (
-    <footer>
+    <footer className={isDark ? "header black" : "header"}>
       <h3>Movie App</h3>
     </footer>
   );
