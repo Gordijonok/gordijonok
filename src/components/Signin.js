1039f98a67cb4afc44d7f31134ae3d79a9b017c6
Gordijonok <126864195+Gordijonok@users.noreply.github.com>
2023-09-21 17:42:31 +0300

    регистрация (#7)
    * регистрация

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,23 +1,83 @@
+import { useForm } from "react-hook-form";
+
+import { useState } from "react";
+
+import { useNavigate } from "react-router-dom";
+
+import { regExp } from "./const/const";
+
 function Signin() {
+  const navigate = useNavigate();
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    mode: "onBlur",
+  });
+  const [error, setError] = useState({});
+  const onSign = (data) => {
+    const dataStorage = JSON.parse(localStorage.getItem("users"));
+    if (dataStorage !== null) {
+      dataStorage.forEach((item) => {
+        if (item.email === data.email && item.password === data.password) {
+          navigate("/");
+        }
+      });
+      if (!dataStorage.find((item) => item.email === data.email)) {
+        setError({ email: "User with this email does not exist" });
+        return;
+      }
+      if (!dataStorage.find((item) => item.password === data.password)) {
+        setError({ password: "Invalid password" });
+      }
+    } else {
+      setError({ email: "User with this email does not exist" });
+    }
+  };
+
   return (
     <div className="main">
       <div className="main__signin">
-        <form className="signin" action="">
+        <form onSubmit={handleSubmit(onSign)} className="signin" action="">
           <h5 className="signin__title">Sign in</h5>
           <label htmlFor="signinEmail">E-mail</label>
           <input
+            {...register("email", {
+              required: " This field cannot be empty",
+              pattern: {
+                value: regExp,
+                message: "Email is not valid",
+              },
+            })}
             id="signinEmail"
-            name="inemail"
+            name="email"
             type="text"
             placeholder="Your email"
           />
+
+          {errors?.email && <p className="errors">{errors?.email?.message}</p>}
+          {error && <p className="errors">{error?.email}</p>}
+
           <label htmlFor="signinPassword">Password</label>
           <input
+            {...register("password", {
+              required: "This field cannot be empty",
+              minLength: {
+                value: 5,
+                message: "Password is too short",
+              },
+            })}
             id="signinPassword"
-            name="inpassword"
-            type="text"
+            name="password"
+            type="password"
             placeholder="Your password"
           />
+          {errors?.password && (
+            <p className="errors">{errors?.password?.message}</p>
+          )}
+          {error && <p className="errors">{error?.password}</p>}
           <button className="signin__btn" type="submit">
             Sign in
           </button>
---
+++ src/redux/movieApi.js
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+import { API_KEY } from "../components/const/const";
+
+export const movieApi = createApi({
+  reducerPath: "kinopoisk/api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://www.omdbapi.com/",
+  }),
+  endpoints: (builder) => ({
+    getOneMovie: builder.query({
+      query: (query) => `?apikey=${API_KEY}&i=${query}`,
+    }),
+  }),
+});
+
+export const { useGetOneMovieQuery } = movieApi;
