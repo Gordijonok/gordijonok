4662bc5efb9f504577702d229c1c6edd7aff95cf
Gordijonok <dkflbvbhujhlb@mail.ru>
2023-09-25 18:24:27 +0300

    правки

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -23,17 +23,17 @@ function HeaderNav() {
       {isAuthorized ? (
         <ul className="nav__list">
           <h2 className="username"> Hello, {currentUser.login}</h2>
-          <li className="nav__elem" key={1}>
+          <li className="nav__elem">
             <Link className="white" to="/favourite">
               Favourite
             </Link>
           </li>
-          <li className="nav__elem" key={2}>
+          <li className="nav__elem">
             <Link className="white" to="/history">
               History
             </Link>
           </li>
-          <li onClick={exitUser} className="nav__elem" key={3}>
+          <li onClick={exitUser} className="nav__elem">
             <Link className="white" to="/">
               Log out
             </Link>
@@ -41,12 +41,12 @@ function HeaderNav() {
         </ul>
       ) : (
         <ul className="nav__list">
-          <li className="nav__elem" key={4}>
+          <li className="nav__elem">
             <Link className="white" to="/signin">
               Sign in
             </Link>
           </li>
-          <li className="nav__elem" key={5}>
+          <li className="nav__elem">
             <Link className="white" to="/signup">
               Sign up
             </Link>
